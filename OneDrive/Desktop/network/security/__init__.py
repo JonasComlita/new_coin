5586263d5a5b1e12035f7ed6b4233a955b438786
JonasComlita <153332326+JonasComlita@users.noreply.github.com>
2025-03-18 17:41:13 -0700

    Delete OneDrive/Desktop/network directory

--- OneDrive/Desktop/network/security/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-print("security/__init__.py being imported from:", os.getcwd())
-
-from .monitor import SecurityMonitor
-from .mfa import MFAManager
-from .backup import KeyBackupManager
-
-__all__ = ['SecurityMonitor', 'MFAManager', 'KeyBackupManager'] 
